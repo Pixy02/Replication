a6d49e059c9f1f02c89af898ede4c3ab618c3780
Irfan Muhammad_02 <72332713+Pixy02@users.noreply.github.com>
2021-10-10 22:02:37 +0700

    Add files via upload

---
+++ Player Data/Packet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Player_Data
+{
+    public enum Packet
+    {
+        Spawn = 1,
+        Move,
+        Attack,
+    }
+
+    class PacketData
+    {
+        private List<byte> buffer;
+        private byte[] readableBuffer;
+        private int readPos;
+    }
+}
---
+++ Player Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Player_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
